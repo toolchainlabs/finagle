15e64796390ce5035cffe7d7a16b2386f4768ea7
Stu Hood <stuhood@gmail.com>
2021-11-14 16:06:52 -0800

    Initialize empty thirdparty BUILD files.

--- create-thirdparty.sh
+++ create-thirdparty.sh
@@ -2,9 +2,29 @@
 
 set -eu
 
+util_build_file="3rdparty/jvm/com/twitter/BUILD.v2"
+cat << EOF > "${util_build_file}"
+UTIL_VERSION = 'TODO'
+
+def util_lib(name):
+    artifact_name = f'util-{name}_2.13'
+    jvm_artifact(
+      name=artifact_name,
+      group='com.twitter',
+      artifact=artifact_name,
+      version=UTIL_VERSION,
+      packages=[f'com.twitter.{name}.**'],
+    )
+
+EOF
+
+for n in $(rg -o 'util\("[^)]*"\)' build.sbt | sort -u | awk -F\" '{print $2 }'); do
+  echo "util_lib('${n}')" >> "${util_build_file}"
+done
+
 for t in $(rg --glob='BUILD' -I -o '3rdparty/jvm/[^"]*' | sort -u); do
   dir=$(echo "$t" | awk -F: '{print $1}')
-  build_file_name="${dir}/BUILD"
+  build_file_name="${dir}/BUILD.v2"
   group=$(echo "$dir" | cut -c 14- | tr '/' '.')
 
   target_name=$(echo "$t" | awk -F: '{print $2}')
